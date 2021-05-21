810ecb53e8db07b8e04b74fc9b533840968902fd
sora0420 <thfk0420@naver.com>
2021-05-21 09:31:08 +0900

    angularjs

---
+++ front-end/controllers/exam05Controller.js
@@ -0,0 +1,6 @@
+angular.module("app")
+    .controller("exam05Controller", function($scope){
+        console.log("app.exam05Controller instanciation");
+        $scope.controllerName="app.exam05Controller";
+    });
+    
