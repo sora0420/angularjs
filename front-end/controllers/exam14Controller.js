8601e49216f10af5c8a1540d06188bce47b9d6c6
sora0420 <thfk0420@naver.com>
2021-06-18 10:08:25 +0900

    angularjs

---
+++ front-end/controllers/exam14Controller.js
@@ -0,0 +1,10 @@
+angular.module("app") //app모듈을 가져옴
+  .controller("exam14Controller", function($rootScope, $scope){ //function은 생성자
+    $scope.todos = [
+      {action:"동영상 녹화(Office)", complete: false}, //할 일에 대한 객체 //목록을 만든다 하면 배열로 가져와야함/ 서버에서
+      {action:"앵귤러JS 복습(Home)", complete: false}, //객체 3개
+      {action:"알마인드(Home)", complete: false}, //repeat하면 3개 또 repeat하면 속성 반복해서 2개
+      {action:"밥먹기(Home)", complete: false}
+    ];
+  });
+
