189b07004d86e7d24e39b4f999fe20b6bb0ae24d
sora0420 <thfk0420@naver.com>
2021-06-22 21:00:52 +0900

    angularjs

---
+++ front-end/controllers/exam16Controller.js
@@ -0,0 +1,15 @@
+angular.module("app") //app모듈을 가져옴
+  .controller("exam16Controller", function($rootScope, $scope){ //function은 생성자
+    $scope.view = { //객체
+      modes: ["none", "table", "list"], //속성 2개 models / mode
+      mode: "none"
+    };
+
+    $scope.todos = [
+      {action:"동영상 녹화(Office)", complete: false}, //할 일에 대한 객체 //목록을 만든다 하면 배열로 가져와야함/ 서버에서
+      {action:"앵귤러JS 복습(Home)", complete: false}, //객체 4개
+      {action:"알마인드(Home)", complete: false}, //repeat하면 4개 또 repeat하면 속성 반복해서 2개
+      {action:"밥먹기(Office)", complete: false}
+    ];
+  });
+
